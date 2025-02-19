fe4ae97f774ae7e160dc10b51529dcc81c75ea51
connor <connor.alexander89@outlook.com>
2025-02-19 12:53:14 -0600

    fix router and base url config

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <CssBaseline />
-      <Router>
+      <Router basename="/CYB70-security-training-module">
         <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
         <MainContent />
       </Router>
